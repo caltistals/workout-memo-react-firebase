00fd2f46caccb23878415115878f1064e5b7c262
caltistals <y.db.qp.y@gmail.com>
2022-07-10 22:15:54 +0900

    update Tobbarのスタイリングの調整

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import "./Topbar.css"
 import HomeIcon from '@mui/icons-material/Home';
-// import AddAlarmIcon from '@mui/icons-material/AddAlarm';
+import AddAlarmIcon from '@mui/icons-material/AddAlarm';
 import { NavLink } from 'react-router-dom';
 import { auth } from "../../firebase.js"
 
@@ -11,15 +11,18 @@ function Topbar() {
   return (
     <div className="topbarContainer">
         <h3 className="logo">Workout memo</h3>
-        <div className="topbarCenter">
-          <NavLink exact to = "/" className = {({isActive}) => isActive ? "is-active": "homeMenu"}>
-                <HomeIcon/>ホーム
-          </NavLink>
-          {/* <NavLink exact to = "/records" className = {({isActive}) => isActive ? "is-active": "recordMenu"}>
-                <AddAlarmIcon/> 記録
-          </NavLink> */}
-
-        </div>
+        <ul className="topbarNav">
+          <li>
+            <NavLink exact to = "/" className = {({isActive}) => isActive ? "is-active": "homeMenu"}>
+                  <HomeIcon/>ホーム
+            </NavLink>
+          </li>
+          {/* <li>
+            <NavLink exact to = "/records" className = {({isActive}) => isActive ? "is-active": "recordMenu"}>
+                  <AddAlarmIcon/> 記録
+            </NavLink>
+          </li> */}
+        </ul>
         <div className="topbarRight">
           <div className="logout">
             <button className='logoutButton' onClick={() => auth.signOut()} >ログアウト</button>
